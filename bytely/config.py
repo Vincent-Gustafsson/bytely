d34811b235ee125017c2fa5de863d3b75deddb7e
Vincent Gustafsson <vincenthonor8lite@gmail.com>
2020-04-12 13:56:59 +0200

    Added secret key generation.

--- bytely/config.py
+++ bytely/config.py
@@ -1,7 +1,8 @@
 """App config"""
+import os
 
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'sahDKAW6276327813jdisjdj293190#¤%&/'
+    SECRET_KEY = os.urandom(24).hex()
